d50381370fad9863c2f372db3675204313bd2068
Jared <jared.watson@bunn.com>
2022-11-18 08:33:44 -0600

    add more animation and clean up js

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+// Control navigation animation
+function navAnimation(direction1, direction2) {
+    navItems.forEach((nav, i) => {
+        nav.classList.replace(`slide-${direction1}-${i + 1}`, `slide-${direction2}-${i + 1}`);
+    });
+}
 
 function toggleNav() {
     // Toggle: Menu Bars Open/Close
@@ -13,18 +21,19 @@ function toggleNav() {
     overlay.classList.toggle('overlay-active');
     if (overlay.classList.contains('overlay-active')) {
         // Animate in - Overlay
-        overlay.classList.add('overlay-slide-right');
-        overlay.classList.remove('overlay-slide-left');
+        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
+        // Animate in - Nav Items
+        navAnimation('out', 'in')
     } else {
-        overlay.classList.add('overlay-slide-left');
-        overlay.classList.remove('overlay-slide-right');
+        // Animate out - Overlay
+        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
+        // Animate out - Nav Items
+        navAnimation('in', 'out')
     }
 }
 
 // Event Listeners
 menuBars.addEventListener('click', toggleNav);
-nav1.addEventListener('click', toggleNav);
-nav2.addEventListener('click', toggleNav);
-nav3.addEventListener('click', toggleNav);
-nav4.addEventListener('click', toggleNav);
-nav5.addEventListener('click', toggleNav);
+navItems.forEach((nav) => {
+    nav.addEventListener('click', toggleNav);
+});
