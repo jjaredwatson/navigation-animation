38add9f394c0fe5826f925cc9980f9079c559cd5
Jared <jared.watson@bunn.com>
2022-11-17 19:40:27 -0600

    add js functionality and css classes for burger menu

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+
+function toggleNav() {
+    // Toggle: Menu Bars Open/Close
+    menuBars.classList.toggle('change');
+    // Toffle: Menu Active
+    overlay.classList.toggle('overlay-active');
+    if (overlay.classList.contains('overlay-active')) {
+        // Animate in - Overlay
+        overlay.classList.add('overlay-slide-right');
+        overlay.classList.remove('overlay-slide-left');
+    } else {
+        overlay.classList.add('overlay-slide-left');
+        overlay.classList.remove('overlay-slide-right');
+    }
+}
+
+// Event Listeners
+menuBars.addEventListener('click', toggleNav);
+nav1.addEventListener('click', toggleNav);
+nav2.addEventListener('click', toggleNav);
+nav3.addEventListener('click', toggleNav);
+nav4.addEventListener('click', toggleNav);
+nav5.addEventListener('click', toggleNav);
